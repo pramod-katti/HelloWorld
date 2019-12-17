f5cb1b11cd306d4073d53db78f8b1e3392049afe
Pramod Katti <53078632+pramod-katti@users.noreply.github.com>
2019-12-17 22:16:13 +0530

    Different Types of Hello World

--- Helloworld.py
+++ Helloworld.py
@@ -1,4 +1,28 @@
-hello = ("Hello World")
+#1
+helo = ("Hello World")
 print(hello)
-name = input("What is your name?")
-print("Hello! %s"%name)
+
+#2
+print("Hello world")
+
+#3
+print("Hello "+"World")
+
+#4
+print("Hello","world")
+
+#5
+def greet(to_whom):
+  """(word) -> str
+  This function takes a word and prints it along with Hello
+  Examples:
+  >>> greet(India)
+  Hello India
+  >>> greet(Aravind)
+  Hello Aravind
+  """
+  print("Hello",to_whom)
+  return
+
+#6
+
